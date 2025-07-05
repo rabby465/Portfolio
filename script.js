be7d58fb14f479860ed8b3223adbac05f8c942ef
rabby465 <rabbiiaahmed@gmail.com>
2025-07-06 02:13:53 +0600

    main file

---
+++ script.js
@@ -0,0 +1,270 @@
+  const videos = [
+    {
+      title: "Promotional Video",
+      description: "In this video, I used  Premier Pro and After Effects",
+      videoId: "APKYx4Uts6U",
+      type: "Widescreen - 16:9"
+    },
+        {
+      title: "Promotional Video",
+      description: "In this video, I used  Premier Pro and After Effects",
+      videoId: "H9ueWO_FC4M",
+      type: "Widescreen - 16:9"
+    },
+     
+       {
+      title: "Promotional Video",
+      description: "In this video, I used  Premier Pro and After Effects",
+      videoId: "FyQfQEBlJbM",
+      type: "Widescreen - 16:9"
+    },
+     
+       {
+      title: "Promotional Video",
+      description: "In this video, I used  Premier Pro and After Effects",
+      videoId: "1UQwC_14jV0",
+      type: "Widescreen - 16:9"
+    },
+     
+       {
+      title: "Promotional Video",
+      description: "In this video, I used  Premier Pro and After Effects",
+      videoId: "9LQRGQ9dm14",
+      type: "Widescreen - 16:9"
+    },
+     
+       {
+      title: "Promotional Video",
+      description: "In this video, I used  Premier Pro and After Effects",
+      videoId: "aS99QHyZXpY",
+      type: "Widescreen - 16:9"
+    },
+     
+       {
+      title: "Promotional Video",
+      description: "In this video, I used  Premier Pro and After Effects",
+      videoId: "SN46KigPML4",
+      type: "Widescreen - 16:9"
+    },
+     
+       {
+      title: "Promotional Video",
+      description: "In this video, I used  Premier Pro and After Effects",
+      videoId: "JyjimDqBkPM",
+      type: "Widescreen - 16:9"
+    },
+     
+       {
+      title: "Promotional Video",
+      description: "In this video, I used  Premier Pro and After Effects",
+      videoId: "KE5JY4JYP98",
+      type: "Widescreen - 16:9"
+    },
+     
+          {
+      title: "Promotional Video",
+      description: "In this video, I used  Premier Pro and After Effects",
+      videoId: "U9lAjosCTEY",
+      type: "Widescreen - 16:9"
+    },
+     
+   
+          {
+      title: "Promotional Video",
+      description: "In this video, I used  Premier Pro and After Effects",
+      videoId: "JzNycOdcy2M",
+      type: "Widescreen - 16:9"
+    },
+     
+   
+          {
+      title: "Promotional Video",
+      description: "In this video, I used  Premier Pro and After Effects",
+      videoId: "QZEdCRPl5eU",
+      type: "Widescreen - 16:9"
+    },
+     
+   
+          {
+      title: "Promotional Video",
+      description: "In this video, I used  Premier Pro and After Effects",
+      videoId: "XyEnoCtD2qk",
+      type: "Widescreen - 16:9"
+    },
+     
+   
+     
+    {
+      title: "Reels",
+      description: "In this video, I used Premier Pro & After Effect.",
+      videoId: "9RkXhAxevSo",
+      type: "Reels - 9:16"
+    },
+      {
+      title: "Reels",
+      description: "In this video, I used Premier Pro & After Effect.",
+      videoId: "RMEEZS0bs90",
+      type: "Reels - 9:16"
+    },
+   
+      {
+      title: "Reels",
+      description: "In this video, I used Premier Pro & After Effect.",
+      videoId: "keu_vBPF8d4",
+      type: "Reels - 9:16"
+    },
+   
+      {
+      title: "Reels",
+      description: "In this video, I used Premier Pro & After Effect.",
+      videoId: "cOaFX_MHUek",
+      type: "Reels - 9:16"
+    },
+   
+      {
+      title: "Reels",
+      description: "In this video, I used Premier Pro & After Effect.",
+      videoId: "_AUd-adPax8",
+      type: "Reels - 9:16"
+    },
+   
+      {
+      title: "Reels",
+      description: "In this video, I used Premier Pro & After Effect.",
+      videoId: "WSpXfWj0HA4",
+      type: "Reels - 9:16"
+    },
+   
+      {
+      title: "Reels",
+      description: "In this video, I used Premier Pro & After Effect.",
+      videoId: "NMt_Aelitms",
+      type: "Reels - 9:16"
+    },
+   
+      {
+      title: "Reels",
+      description: "In this video, I used Premier Pro & After Effect.",
+      videoId: "qldOwkHsQ-U",
+      type: "Reels - 9:16"
+    },
+   
+      {
+      title: "Reels",
+      description: "In this video, I used Premier Pro & After Effect.",
+      videoId: "8DHPewlim2o",
+      type: "Reels - 9:16"
+    },
+     {
+      title: "Reels",
+      description: "In this video, I used Premier Pro & After Effect.",
+      videoId: "sdqR6M7-KiI",
+      type: "Reels - 9:16"
+    },
+     {
+      title: "Reels",
+      description: "In this video, I used Premier Pro & After Effect.",
+      videoId: "5zJltf6g4Dk",
+      type: "Reels - 9:16"
+    },
+   
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    {
+    
+      title: "Motion Graphics",
+      description: "In this video, I used Photoshop & After Effect & Premier Pro",
+      videoId: "ACqpJFA-SnI-Dk",
+      type: "Square - 1:1"
+    },
+    {
+    
+      title: "Motion Graphics",
+      description: "In this video, I used Photoshop & After Effect & Premier Pro",
+      videoId: "JxJwO7If-0w",
+      type: "Square - 1:1"
+    },
+ 
+       {
+    
+      title: "Motion Graphics",
+      description: "In this video, I used Photoshop & After Effect & Premier Pro",
+      videoId: "2XkkZK1upvs",
+      type: "Square - 1:1"
+    },
+ 
+       {
+    
+      title: "Motion Graphics",
+      description: "In this video, I used Photoshop & After Effect & Premier Pro",
+      videoId: "7u2eyhckaPE",
+      type: "Square - 1:1"
+    },
+ 
+       {
+    
+      title: "Motion Graphics",
+      description: "In this video, I used Photoshop & After Effect & Premier Pro",
+      videoId: "tSD746u7YAs",
+      type: "Square - 1:1"
+    },
+ 
+       {
+    
+      title: "Motion Graphics",
+      description: "In this video, I used Photoshop & After Effect & Premier Pro",
+      videoId: "3krAVjSGbls",
+      type: "Square - 1:1"
+    },
+ 
+       {
+    
+      title: "Motion Graphics",
+      description: "In this video, I used Photoshop & After Effect & Premier Pro",
+      videoId: "ETt8wu-ihzQ",
+      type: "Square - 1:1"
+    },
+ 
+    
+    
+  ];
+
+
+  
+  const ratioMap = {
+    "Widescreen - 16:9": "pt-[56.25%]",
+    "Reels - 9:16": "pt-[177.77%]",
+    "Instagram Feed - 4:5": "pt-[125%]",
+    "Square - 1:1": "pt-[100%]"
+  };
+
+
+
+  const images  = [
+    'testimonial/tes1.png',
+    'testimonial/tes2.png',
+    'testimonial/tes3.png',
+    'testimonial/tes4.png',
+    'testimonial/tes5.png',
+    'testimonial/tes6.png'
+  ];
